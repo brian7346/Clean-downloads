250f19c5ca0bd5f5f4c0714e845d03a1a15e7f25
Nathan Menge <nathan.menge@gmail.com>
2020-03-17 13:42:51 -0400

    [Rewrite] Feel free to ask questions.

--- cleanup.rb
+++ cleanup.rb
@@ -1,53 +1,59 @@
+require 'file'
 require 'fileutils'
 
-puts 'Enter PC username: '
-username = gets.chomp
-path = "/Users/#{username}/Downloads"
-
-def image? str 
- str.match?(/png|jpg|svg|jpeg/)
-end
-
-def exe? file
- file.match?(/exe|dmg/)
-end
-
-def move_images path, file
-  if !Dir.exist? "Images"
-    begin
-      FileUtils.mkdir_p 'Images'
-      FileUtils.mv("#{path}/#{file}", "#{path}/Images")
-    rescue
-      puts 'Something went wrong!'
-    end 
-  else
-    begin
-      FileUtils.mv("#{path}/#{file}", "#{path}/Images")
-    rescue
-      puts 'Something went wrong!'
+class DownloadCleaner
+  def initialize(user_root)
+    @root = user_root
+    paths = {image: File.join(@root, 'Downloads', 'Images'),
+             executable: File.join(@root, 'Downloads', 'Executables')}
+    #make sure your paths exists right away, rather than mid file loop
+    paths.each_value do |path|
+      FileUtils.mkdir_p(path) unless Dir.exist?(path)
     end
+  end
+
+  def move_file(file_path)
+    begin
+      dest = find_dest(file_path)
+    rescue ArgumentError
+      puts "File: #{file_path} was not moved"
+    else
+      # I'm specifically NOT rescuing on FileUtils.mv, in the event of an exception I want that to come to the terminal
+      # rescuing it and saying "something went wrong" gives you absolutely no info as to what went wrong
+      FileUtils.mv(file_path, dest)
+      puts "File: #{file_path} moved to #{dest}"
+    end
+  end
+
+  def move_files
+    download_path = File.join(@root, 'Downloads')
+    Dir.each_child(download_path) { |file| move_file(File.join(download_path, file)) }
+  end
+
+  def self.executable?(file)
+    file.match?(/exe|dmg/)
+  end
+
+  def self.image?(str)
+    str.match?(/png|jpg|svg|jpeg/)
+  end
+
+  private
+
+  def find_dest(file_path)
+    return paths[:image] if self.class.image?(file_path)
+    return paths[:executable] if self.class.executable?(file_path)
+    raise ArgumentError, "Path: #{file_path} is neither an image nor an executable"
   end
 end
 
-def move_exe path, file
-  if !Dir.exist? "Executable"
-    FileUtils.mkdir_p 'Executable'
-    FileUtils.mv("#{path}/#{file}", "#{path}/Executable")
-  else
-    FileUtils.mv("#{path}/#{file}", "#{path}/Executable")
-  end
-end
-
-if Dir.exist? path
-  Dir.chdir path
-  Dir.each_child(path) { |file|
-    if image? file 
-      move_images path, file
-    elsif exe? file
-      move_exe path, file
-    end
-  }
-  puts 'Done!'
+# rather than do IO with the user, just expect them to enter the username they want to clean
+if ARGV.length == 1
+  user_path = File.join('/Users', ARGV[0])
+  cleaner = DownloadCleaner.new(user_path)
+  cleaner.move_files
+  puts "Done"
 else
-  puts 'Incorrect PC username'
+  puts "Usage - 'ruby cleanup.rb <username>'"
 end
+
