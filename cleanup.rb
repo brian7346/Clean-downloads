c35feddf2117ece3cb080b6f8ec9a51b18dcb601
Maxim Roslow <brian2009@yandex.ru>
2020-03-19 21:24:09 +0400

    add class

--- cleanup.rb
+++ cleanup.rb
@@ -1,62 +1,40 @@
 require 'fileutils'
 require 'OS'
 
-print 'Enter PC username: '
-username = gets.chomp
+class DownloadCleaner 
+  def initialize
+    @config = {
+      :os => nil,
+      :drive => nil,
+      :username => nil,
+      :path => nil
+    }
 
-def path username
-  if OS.windows?
-    return "C:/Users/#{username}/Downloads"
-  end
+    print 'Enter PC username: '
+    username = gets.chomp
+    @config[:username] = username
 
-  "/Users/#{username}/Downloads"
-end
-
-def image? str 
- str.match?(/png|jpg|svg|jpeg/)
-end
-
-def exe? file
- file.match?(/exe|dmg/)
-end
-
-def move_images local_path, file
-  if !Dir.exist? "Images"
-    begin
-      FileUtils.mkdir_p 'Images'
-      FileUtils.mv("#{local_path}/#{file}", "#{local_path}/Images")
-    rescue
-      puts 'Something went wrong!'
-    end 
-  else
-    begin
-      FileUtils.mv("#{local_path}/#{file}", "#{local_path}/Images")
-    rescue
-      puts 'Something went wrong!'
+    if OS.windows?
+      windows
+    elsif OS.posix?
+      unix
     end
+
+    puts @config
+  end
+
+  def windows 
+    @config[:os] = 'windows'
+
+    print 'Enter Local Drive: '
+    drive = gets.chomp.upcase!
+
+    @config[:drive] = "#{drive}:/"
+  end
+
+  def unix 
+    @config[:os] = 'unix'
   end
 end
 
-def move_exe local_path, file
-  if !Dir.exist? "Executable"
-    FileUtils.mkdir_p 'Executable'
-    FileUtils.mv("#{local_path}/#{file}", "#{local_path}/Executable")
-  else
-    FileUtils.mv("#{local_path}/#{file}", "#{local_path}/Executable")
-  end
-end
-
-if Dir.exist? path(username)
-  Dir.chdir path(username)
-
-  Dir.each_child(path(username)) { |file|
-    if image? file 
-      move_images path(username), file
-    elsif exe? file
-      move_exe path(username), file
-    end
-  }
-  puts 'Done!'
-else
-  puts 'Incorrect PC username'
-end
+DownloadCleaner.new
